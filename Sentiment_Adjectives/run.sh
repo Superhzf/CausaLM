4d9cb05e42f863083dc9512af35c9f2c823918c5
Superhzf <superhzf@hotmail.com>
2023-02-12 21:19:27 -0600

    type: feat Test the conda env

---
+++ Sentiment_Adjectives/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem=64G
+#SBATCH --time=12:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:rtx_6000:1
+
+cd /work/LAS/cjquinn-lab/zefuh/CausaLM/CausaLM
+
+module load miniconda3/4.10.3-svrr7oe
+
+source activate causalm
